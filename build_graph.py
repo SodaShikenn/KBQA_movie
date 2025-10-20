5215b8249ba2b4ea9d4d3c6c98def5d0fcaa1714
SodaShikenn <zixuanhe43@gmail.com>
2025-10-20 20:11:01 +0900

    Assemble relational data and cache entity data

--- build_graph.py
+++ build_graph.py
@@ -28,9 +28,37 @@ class BuildGraph():
                 '上映': '/'.join(movie['pubdates']),
                 '片长': '/'.join(movie['durations']),
             })
-            print(self.entity_data)
-            exit()
+            # 导演
+            for director in movie['directors']:
+                print(movie['directors'])
 
-            
+                director_name = director[1]
+                dct = {'name': director_name}
+                if dct not in self.entity_data['PERSON']:
+                    self.entity_data['PERSON'].append(dct)
+                self.relation_data['导演'].append((movie_name, director_name))
+            # 编剧
+            for writer in movie['writers']:
+                writer_name = writer[1]
+                dct = {'name': writer_name}
+                if dct not in self.entity_data['PERSON']:
+                    self.entity_data['PERSON'].append(dct)
+                self.relation_data['编剧'].append((movie_name, writer_name))
+            # 主演
+            for actor in movie['actors']:
+                actor_name = actor[1]
+                dct = {'name': actor_name}
+                if dct not in self.entity_data['PERSON']:
+                    self.entity_data['PERSON'].append(dct)
+                self.relation_data['主演'].append((movie_name, actor_name))
+    # 缓存实体文件
+    def dump_entitys(self):
+        entitys = []
+        for label, entity_list in self.entity_data.items():
+            entitys += [entity['name'] for entity in entity_list]
+        with open(ENTITYS_PATH, 'w', encoding='utf-8') as file:
+            file.write('\n'.join(entitys))
+
 if __name__ == '__main__':
     bg = BuildGraph()
+    bg.dump_entitys()
