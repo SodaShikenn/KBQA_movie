26de5884ca2105973ca976d422a7a3f713d9642c
SodaShikenn <zixuanhe43@gmail.com>
2025-10-21 00:02:55 +0900

    Realize Lexical Slot Function

--- demo.py
+++ demo.py
@@ -1,6 +1,136 @@
-# 例1：霸王别姬的主演有哪些？
-{'%ENT%': ['霸王别姬'], '%ATT%': [], '%REL%': ['主演']}
-#例2：霸王别姬的片长是多少？
-{'%ENT%': ['霸王别姬'], '%ATT%': ['片长'], '%REL%': []}
-# 例3：霸王别姬和张国荣是什么关系？
-{'%ENT%': ['张国荣', '霸王别姬'], '%ATT%': [], '%REL%': []}
+question = '%ENT%的%REL%是谁？'
+combination = {'%ENT%':'霸王别姬', '%REL%':'导演'}
+
+def replace_token_in_string(string, combination):
+    for key, value in combination.items():
+        string = string.replace(key, value)
+    return string
+
+res = replace_token_in_string(question, combination)
+print(res)
+
+exit()
+
+
+def check_slots(cypher_slots, slots):
+    for k, v in cypher_slots.items():
+        if v > len(slots[k]):
+            return False
+    return True
+
+
+cypher_slots = {
+    "%ENT%": 3,
+    "%REL%": 1,
+}
+
+slots = {
+    "%ENT%": ['霸王别姬', '张国荣'],
+    "%REL%": ['主演', '导演']
+}
+
+print(check_slots(cypher_slots, slots))
+exit()
+
+import itertools
+
+
+def get_slots_combinations(cypher_slots, slots):
+    value_combinations = []
+    for k,v in cypher_slots.items():
+        value_combinations.append([*itertools.permutations(slots[k], v)])
+    combinations = []
+    for value_combination in itertools.product(*value_combinations):
+        result = {}
+        for value, key in zip(value_combination, cypher_slots):
+            if len(value) == 1:
+                result[key] = value[0]
+            else:
+                for i in range(len(value)):
+                    result[key[:-1]+str(i)+'%'] = value[i]
+        combinations.append(result)
+    return combinations
+
+cypher_slots = {
+    "%ENT%": 2,
+    "%REL%": 1,
+}
+
+slots = {
+    "%ENT%": ['霸王别姬', '张国荣'],
+    "%REL%": ['主演', '导演']
+}
+
+result = get_slots_combinations(cypher_slots, slots)
+print(result)
+
+exit()
+
+
+
+lst3 = [(('张国荣', '霸王别姬'), ('导演',)), (('张国荣', '霸王别姬'), ('主演',)), (('霸王别姬', '张国荣'), ('导演',)), (('霸王别姬', '张国荣'), ('主演',))]
+lst4 = ['%ENT%', '%REL%']
+# 期望结果：[{%ENT0%:'张国荣', %ENT1%:'霸王别姬', '%REL%':'导演'}, ...]
+
+result = []
+for item in lst3:
+    dct = {}
+    for value, key in zip(item, lst4):
+        if len(value)==1:
+            dct[key] = value[0]
+        else:
+            for i in range(len(value)):
+                dct[key[:-1]+str(i)+'%'] = value[i]
+    result.append(dct)
+print(result)
+
+exit()
+
+
+import itertools
+
+
+lst1 = [('张国荣', '霸王别姬'), ('霸王别姬', '张国荣')]
+lst2 = [('导演',), ('主演',)]
+
+res = itertools.product(lst1, lst2)
+print(list(res))
+exit()
+
+
+lst = ['张国荣', '霸王别姬', '周星驰']
+res = itertools.permutations(lst, 1)
+print(list(res))
+
+
+exit()
+
+
+import re
+
+quesiton = '霸王别姬和功夫有共同的主演吗？'
+
+lst = ['霸王别姬', '功夫', '阿甘正传', '阿凡达(Avatar)']
+
+res = re.findall('|'.join(lst).replace('(', '').replace(')', ''), quesiton)
+
+print(res)
+
+exit()
+
+
+from collections import defaultdict
+
+entity_data = defaultdict(list)
+entity_data['MOVIE'].append('霸王别姬')
+entity_data['PERSON'].append('张国荣')
+print(entity_data['MOVIE'][0])
+exit()
+
+
+entity_data = {}
+entity_data['MOVIE'] = []
+entity_data['MOVIE'].append('霸王别姬')
+entity_data['PERSON'] = []
+entity_data['PERSON'].append('张国荣')
+print(entity_data)
--- question_match.py
+++ question_match.py
@@ -1,16 +1,9 @@
 from config import *
-import re
+import re, json, itertools
 
 class GraphQA():
     def __init__(self):
         pass
-
-    def query(self, text):
-        slots = self.get_mention_slots(text)
-        print(slots)
-
-    def get_mention_slots(self, text):
-        entities = self.parse_mention_entities(text)
 
     def parse_mention_entities(self, text):
         with open(ENTITIES_PATH, encoding='utf-8') as file:
@@ -34,14 +27,61 @@ class GraphQA():
         entities = self.parse_mention_entities(text)
         attributes = self.parse_mention_attributes(text)
         relations = self.parse_mention_relations(text)
-        print(attributes)
         return {
             '%ENT%': entities,
             '%ATT%': attributes,
             '%REL%': relations
         }
 
+    def get_slots_combinations(self, cypher_slots, slots):
+        value_combinations = []
+        for k,v in cypher_slots.items():
+            value_combinations.append([*itertools.permutations(slots[k], v)])
+        combinations = []
+        for value_combination in itertools.product(*value_combinations):
+            result = {}
+            for value, key in zip(value_combination, cypher_slots):
+                if len(value) == 1:
+                    result[key] = value[0]
+                else:
+                    for i in range(len(value)):
+                        result[key[:-1]+str(i)+'%'] = value[i]
+            combinations.append(result)
+        return combinations
+    
+    def check_slots(self, cypher_slots, slots):
+        for k, v in cypher_slots.items():
+            if v > len(slots[k]):
+                return False
+        return True
+
+    def replace_token_in_string(self, string, combination):
+        for key, value in combination.items():
+            string = string.replace(key, value)
+        return string
+
+    def expand_templates(self, slots):
+        valid_templates = []
+        for template in TEMPLATES:
+            cypher_slots = json.loads(template['slots'])
+            if self.check_slots(cypher_slots, slots):
+                combinations = self.get_slots_combinations(cypher_slots, slots)
+                for combination in combinations:
+                    question = self.replace_token_in_string(template['question'], combination)
+                    cypher = self.replace_token_in_string(template['cypher'], combination)
+                    answer = self.replace_token_in_string(template['answer'], combination)
+                    valid_templates.append({'question': question, 'cypher': cypher, 'answer': answer})
+        return valid_templates
+
+    # 调用
+    def query(self, text):
+        slots = self.get_mention_slots(text)
+        print(slots)
+        templates = self.expand_templates(slots)
+        print(templates)
+
 if __name__ == '__main__':
     graph_qa = GraphQA()
     answer = graph_qa.query('霸王别姬的片长？')
     answer = graph_qa.query('霸王别姬是谁主演的？')
+    answer = graph_qa.query('张国荣和霸王别姬是什么关系？')
